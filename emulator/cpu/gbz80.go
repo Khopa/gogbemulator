67371ff0a60babf9e89ec240dc7700fd799a372a
Khopa <clemguip@gmail.com>
2025-10-03 00:10:49 +0200

    Added R8 and R16 register types and mappings, implemented easiest ld operations.

--- emulator/cpu/gbz80.go
+++ emulator/cpu/gbz80.go
@@ -11,7 +11,27 @@ type Gbz80 struct {
 	pc uint16 // Program Counter
 }
 
-const A = 0x01 // Accumulator
+type R8Register uint8
+
+const (
+	R8_B  R8Register = 0 // B register
+	R8_C  R8Register = 1 // C register
+	R8_D  R8Register = 2 // D register
+	R8_E  R8Register = 3 // E register
+	R8_H  R8Register = 4 // H register
+	R8_L  R8Register = 5 // L register
+	R8_HL R8Register = 6 // HL register
+	R8_A  R8Register = 7 // Accumulator
+)
+
+type R16Register uint8
+
+const (
+	R16_BC R16Register = 0 // B&C registers
+	R16_DE R16Register = 1 // D&E registers
+	R16_HL R16Register = 2 // H&L registers
+	R16_SP R16Register = 3 // Stack Pointer
+)
 
 /**
  * Create a new instance of the Z80 Registers
@@ -121,4 +141,74 @@ func (gbz80 *Gbz80) SP() uint16 {
 
 func (gbz80 *Gbz80) PC() uint16 {
 	return gbz80.pc
+}
+
+// Setter for 8-bit registers
+func (gbz80 *Gbz80) SetR8Register(reg R8Register, value uint8) {
+	switch reg {
+	case R8_A:
+		gbz80.af = (gbz80.af & 0x00FF) | (uint16(value) << 8)
+	case R8_B:
+		gbz80.bc = (gbz80.bc & 0x00FF) | (uint16(value) << 8)
+	case R8_C:
+		gbz80.bc = (gbz80.bc & 0xFF00) | uint16(value)
+	case R8_D:
+		gbz80.de = (gbz80.de & 0x00FF) | (uint16(value) << 8)
+	case R8_E:
+		gbz80.de = (gbz80.de & 0xFF00) | uint16(value)
+	case R8_H:
+		gbz80.hl = (gbz80.hl & 0x00FF) | (uint16(value) << 8)
+	case R8_L:
+		gbz80.hl = (gbz80.hl & 0xFF00) | uint16(value)
+	}
+}
+
+// Getter for 8-bit registers
+func (gbz80 *Gbz80) GetR8Register(reg R8Register) uint8 {
+	switch reg {
+	case R8_A:
+		return uint8(gbz80.af >> 8)
+	case R8_B:
+		return uint8(gbz80.bc >> 8)
+	case R8_C:
+		return uint8(gbz80.bc & 0x00FF)
+	case R8_D:
+		return uint8(gbz80.de >> 8)
+	case R8_E:
+		return uint8(gbz80.de & 0x00FF)
+	case R8_H:
+		return uint8(gbz80.hl >> 8)
+	case R8_L:
+		return uint8(gbz80.hl & 0x00FF)
+	}
+	return 0
+}
+
+// Setter for 16-bit registers
+func (gbz80 *Gbz80) SetR16Register(reg R16Register, value uint16) {
+	switch reg {
+	case R16_BC:
+		gbz80.af = value
+	case R16_DE:
+		gbz80.bc = value
+	case R16_HL:
+		gbz80.de = value
+	case R16_SP:
+		gbz80.hl = value
+	}
+}
+
+// Getter for 16-bit registers
+func (gbz80 *Gbz80) GetR16Register(reg R16Register) uint16 {
+	switch reg {
+	case R16_BC:
+		return gbz80.af
+	case R16_DE:
+		return gbz80.bc
+	case R16_HL:
+		return gbz80.de
+	case R16_SP:
+		return gbz80.hl
+	}
+	return 0
 }
--- emulator/cpu/ins_misc.go
+++ emulator/cpu/ins_misc.go
@@ -2,6 +2,6 @@ package cpu
 
 // Miscellaneous instructions for the GBZ80 CPU
 
+// No operation, does nothing
 func NOP(gbz80 *Gbz80) {
-	// No operation, does nothing
 }
